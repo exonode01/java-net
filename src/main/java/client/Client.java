af0d9cec5ce1f172d4a19f60c6fad60c6075c49e
Tim Weisbrich <tim.weisbrich@ifd-gmbh.com>
2025-06-23 11:36:21 +0200

    feat: Startklassen hinzugefügt

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,12 +1,8 @@
-/*
- * Copyright (c) 2025 iFD GmbH Chemnitz http://ifd-gmbh.com/
- */
 package client;
 
 /**
  * TODO (TIW) Beschreibung der Klasse ...
  *
- * @author iFD GmbH
  * @author TIW
  */
 public class Client {
