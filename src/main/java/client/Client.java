666dc28446edcb15b26d832bfa247362ae5e5d45
Tim Weisbrich <tim.weisbrich@ifd-gmbh.com>
2025-06-23 12:23:55 +0200

    feat: Kurz auf Tastatur gefallen und hat einfach funktioniert

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,5 +1,14 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import server.Server;
+
 /**
  * TODO (TIW) Beschreibung der Klasse ...
  *
@@ -8,13 +17,33 @@ package client;
  */
 public class Client {
 
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
     private int cIp;
     private int v;
     private char cMac;
     private Server server;
 
-    public Client() {
-        server = new Server();
+    public Client(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        //server = new Server();
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        System.out.println(resp);
+        return resp;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
     }
 
     public void receiveIp() {
@@ -64,18 +93,22 @@ public class Client {
                 na = 0;
                 mask = 24;
         }
-        cIp = server.simpleDhcp(na, mask);
-        System.out.println("IP: 192.168.10." + cIp);
+        //cIp = server.simpleDhcp(na, mask);
+        //System.out.println("IP: 192.168.10." + cIp);
     }
 
-    public void receiveV6() {
+    public void receiveV6() throws IOException {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Eigene Mac:");
-        System.out.println("Link Lokal Adresse:");
+        String mac = scanner.nextLine();
+        String resp = sendMessage("MAC:" + mac);
+        System.out.println("Link Lokal Adresse: " + resp);
     }
 
-    public static void main(String[] args) {
-        Client client = new Client();
-        client.receiveIp();
+    public static void main(String[] args) throws IOException {
+        Client client = new Client("127.0.0.1", 6666);
+        //client.sendMessage("hello server");
         client.receiveV6();
+        //client.receiveIp();
     }
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -7,6 +7,11 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import inet.ipaddr.AddressStringException;
+import inet.ipaddr.MACAddressString;
+import inet.ipaddr.ipv6.IPv6Address;
+import inet.ipaddr.mac.MACAddress;
+
 /**
  * Einfacher Server oder irgend sowas Ähnliches.
  *
@@ -30,12 +35,29 @@ public class Server {
         out = new PrintWriter(socket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String greeting = in.readLine();
+        if (greeting.startsWith("MAC:")) {
+            String mac = greeting.substring(4);
+            String linkLocal = createLinkLocal(mac);
+            out.println(linkLocal);
+        }
         if ("hello server".equals(greeting)) {
             out.println("hello client");
         }
         else {
             out.println("unrecognised greeting");
         }
+    }
+
+    private String createLinkLocal(String macStr) {
+        try {
+            MACAddress mac = new MACAddressString(macStr).toAddress();
+            IPv6Address linkLocal = mac.toLinkLocalIPv6();
+            System.out.println("converted " + mac + " to IPv6 link local " + linkLocal);
+            return String.valueOf(linkLocal);
+        } catch(AddressStringException e) {
+            // handle invalid address string here
+        }
+        return null;
     }
 
     public void stop() throws IOException {
